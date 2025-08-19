a3a794c0ce5b38079fe6d01da3bcf5c3353a3a53
baardl <bard.lind@gmail.com>
2025-08-19 11:16:31 +0200

    Sending WhoIs

---
+++ src/main/java/no/messom/realestate/bacnet/BacnetCOVSubscriber.java
@@ -0,0 +1,58 @@
+package no.messom.realestate.bacnet;
+
+import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.npdu.ip.IpNetwork;
+import com.serotonin.bacnet4j.npdu.ip.IpNetworkBuilder;
+import com.serotonin.bacnet4j.service.unconfirmed.WhoIsRequest;
+import com.serotonin.bacnet4j.transport.DefaultTransport;
+import com.serotonin.bacnet4j.type.enumerated.ObjectType;
+import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
+import org.slf4j.Logger;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class BacnetCOVSubscriber {
+    private static final Logger log = getLogger(BacnetCOVSubscriber.class);
+
+    private static final int LOCAL_DEVICE_ID = 1235;
+    private static final int REMOTE_DEVICE_ID = 2640;
+    private static final String REMOTE_IP = "192.168.2.233";
+    public static final String LOCAL_IP = "192.168.2.28";
+    private static final int BACNET_PORT = 47808;
+    private static final int COV_LIFETIME = 60; // seconds
+    private static final ObjectIdentifier TARGET_OBJECT =
+            new ObjectIdentifier(ObjectType.analogValue, 1);
+
+    public static void main(String[] args) throws Exception {
+        BacnetCOVSubscriber app = new BacnetCOVSubscriber();
+        app.run();
+    }
+
+    public void run() throws Exception {
+        LocalDevice localDevice = null;
+        ScheduledExecutorService scheduler = null;
+
+        try {
+            IpNetwork ipNetwork = new IpNetworkBuilder()
+                    .withLocalBindAddress(LOCAL_IP)
+                    .withBroadcast("192.168.2.255", 32)
+                    .build();
+            localDevice = new LocalDevice(LOCAL_DEVICE_ID, new DefaultTransport(ipNetwork));
+            localDevice.initialize();
+            localDevice.getIAm();
+//            localDevice.sendGlobalBroadcast(new WhoIsRequest());
+            localDevice.sendLocalBroadcast(new WhoIsRequest());
+//            localDevice.whoIs(REMOTE_DEVICE_ID, REMOTE_IP, BACNET_PORT);
+            log.info("Local BACnet device initialized with ID: {}", LOCAL_DEVICE_ID);
+        } catch (Exception e) {
+            log.error("Failed to initialize local device", e);
+            if (localDevice != null) {
+                localDevice.terminate();
+            }
+            return;
+        }
+        log.info("Local BACnet device initialized with ID: {}", LOCAL_DEVICE_ID);
+    }
+}
